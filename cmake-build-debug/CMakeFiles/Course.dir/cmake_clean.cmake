7d770e18c618cbdb58ad98aac1d02fc0882dd476
Kerman <grigorij.perfilin@mail.ru>
2022-02-01 21:20:09 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/Course.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Course.dir/main.c.obj"
+  "Course.exe"
+  "Course.exe.manifest"
+  "Course.pdb"
+  "libCourse.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Course.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
